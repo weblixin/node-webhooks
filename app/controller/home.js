4a02d22ec390168dfb83ebb3d072631b682e4a12
lixin <lixin@zhongbaounion.com>
2021-12-15 17:51:39 +0800

    添加打印

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,6 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    console.log('我进来了');
     ctx.body = 'hi, egg';
   }
 
