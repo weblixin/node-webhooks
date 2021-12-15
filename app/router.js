368c9ab517137e6053a55ccd37664d537bce039a
lixin <lixin@zhongbaounion.com>
2021-12-15 18:30:42 +0800

    触发

--- app/router.js
+++ app/router.js
@@ -5,6 +5,7 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+  console.log('开始');
   router.get('/', controller.home.index);
   router.post('/', controller.home.index);
 };
